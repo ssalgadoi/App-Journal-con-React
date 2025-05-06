362d7121a10f0b055e6a714b2b5131e1f7270af2
ssalgadoi <se.salgado@duocuc.cl>
2025-05-06 11:02:34 -0400

    Configuro de MUI a Vite

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,7 +1,13 @@
+import { Typography } from "@mui/material"
+
 
 
 export const JournalPage = () => {
   return (
-    <div>JournalPage</div>
+    <>
+      <Typography variant="h1">JournalPage</Typography>
+
+
+    </>
   )
 }
