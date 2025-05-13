eab570431bc36c993e31929bd31c207ee21e9a86
ssalgadoi <se.salgado@duocuc.cl>
2025-05-12 20:17:15 -0400

    Se crea la configuracion de authSlice

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './styles.css'
 import { JournalApp } from './JournalApp'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { store } from './store'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <Provider store={ store }>
+        <BrowserRouter>
       <JournalApp />
     </BrowserRouter>
-
+    </Provider>
   </StrictMode>,
 )
