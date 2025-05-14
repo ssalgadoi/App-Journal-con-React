fa32e488bdb16b497d5fad35e03e0a3d99cf02fd
ssalgadoi <se.salgado@duocuc.cl>
2025-05-14 10:16:33 -0400

    Funcionalidad para crear nueva nota

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -2,16 +2,26 @@ import { IconButton } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
 import { NoteView, NothingSelectedView } from "../views"
 import { AddOutlined } from "@mui/icons-material"
+import { useDispatch } from "react-redux"
+import { startNewNote } from "../../store/journal"
 
 
 
 export const JournalPage = () => {
+
+  const dispatch = useDispatch();
+
+  const onClickNewNote = () => {
+        dispatch(startNewNote())
+  }
   return (
+
     <JournalLayout>
       
     <NothingSelectedView/> 
     {/* <NoteView/> */}
     <IconButton
+        onClick={ onClickNewNote }
         size="large"
         sx={{
           color: 'white',
